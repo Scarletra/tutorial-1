2e4342ddf3c628b37c443746cee4791e358f1cf2
Scarletra <fernandovalentino10@gmail.com>
2024-02-07 00:37:48 +0700

    fix: Fix missing requirement dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
-:plugins {
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
+plugins {
     java
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
