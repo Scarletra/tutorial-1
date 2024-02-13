21e7cc580bf9c671f230ced359e4c4010753b08a
Scarletra <fernandovalentino10@gmail.com>
2024-02-13 13:59:14 +0700

    Added find product and find by ID test for ProductServiceImpl

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -49,50 +49,50 @@ public class ProductServiceImplTest {
         verify(productRepository, times(1)).create(product);
     }
 
-//    @Test
-//    void testFindAllProduct() {
-//        List<Product> productList = new ArrayList<>();
-//
-//        Product product1 = new Product();
-//        product1.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
-//        product1.setProductName("Sampo Cap Bambang");
-//        product1.setProductQuantity(100);
-//        productList.add(product1);
-//
-//        Product product2 = new Product();
-//        product2.setProductId("6fh123f8-d13a-4e5b-936f-hg192391132");
-//        product2.setProductName("Sampo Cap Xian Yun");
-//        product2.setProductQuantity(100);
-//        productList.add(product2);
-//
-//        Iterator<Product> iterator = productList.iterator();
-//        when(productRepository.findAll()).thenReturn(iterator);
-//
-//        List<Product> result = productServiceImpl.findAll();
-//
-//        assertEquals(productList.size(), result.size());
-//        for (int i = 0; i < productList.size(); i++) {
-//            assertEquals(productList.get(i), result.get(i));
-//        }
-//        verify(productRepository, times(1)).findAll();
-//    }
+    @Test
+    void testFindAllProduct() {
+        List<Product> productList = new ArrayList<>();
+
+        Product product1 = new Product();
+        product1.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+        productList.add(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("6fh123f8-d13a-4e5b-936f-hg192391132");
+        product2.setProductName("Sampo Cap Xian Yun");
+        product2.setProductQuantity(100);
+        productList.add(product2);
+
+        Iterator<Product> iterator = productList.iterator();
+        when(productRepository.findAll()).thenReturn(iterator);
+
+        List<Product> result = productServiceImpl.findAll();
+
+        assertEquals(productList.size(), result.size());
+        for (int i = 0; i < productList.size(); i++) {
+            assertEquals(productList.get(i), result.get(i));
+        }
+        verify(productRepository, times(1)).findAll();
+    }
 
 
-//    @Test
-//    public void testFindById() {
-//        String productId = "6f1238f8-d13a-4e5b-936f-e55156158104";
-//        Product product = new Product();
-//        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
-//        product.setProductName("Sampo Cap Bambang");
-//        product.setProductQuantity(100);
-//
-//        when(productRepository.findById(productId)).thenReturn(product);
-//        Product result = productServiceImpl.findById(productId);
-//        assertEquals(product, result);
-//        verify(productRepository, times(1)).findById(productId);
-//    }
-//
-//
+    @Test
+    public void testFindById() {
+        String productId = "6f1238f8-d13a-4e5b-936f-e55156158104";
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        when(productRepository.findById(productId)).thenReturn(product);
+        Product result = productServiceImpl.findById(productId);
+        assertEquals(product, result);
+        verify(productRepository, times(1)).findById(productId);
+    }
+
+
 //    @Test
 //    void testEditProduct() {
 //        Product product = new Product();
