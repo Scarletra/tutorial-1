4616d573bd39a9e968715272bd182daf40b01fbe
Scarletra <fernandovalentino10@gmail.com>
2024-02-24 15:58:16 +0700

    fix: Remove extend ProductController on CarController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
-
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
