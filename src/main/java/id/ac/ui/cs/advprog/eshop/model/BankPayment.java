2ca23b1f3121be0a7bf9e0c8aa9f8233f36d183f
Scarletra <fernandovalentino10@gmail.com>
2024-03-06 19:46:38 +0700

    [RED] Add BankPayment model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/BankPayment.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class BankPayment extends Payment {
+    
+    public BankPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+    }
+
+    public BankPayment(String id, String method, Order order, Map<String, String> paymentData) {
+    }
+}
